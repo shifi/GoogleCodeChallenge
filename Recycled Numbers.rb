4acf1aabd085b3122a91af70367c89617477d914
marko.siftar@gmail.com <sifi@ubuntu.(none)>
2012-08-31 18:43:33 +0200

    Initial commit

---
+++ Recycled Numbers.rb
@@ -0,0 +1,40 @@
+def split_and_shift(number, size)
+   $hash_num[number]=[]
+   (1...size).each do |i|
+    
+    num_str = number.to_s.split('')
+    num_str << num_str.shift(i)
+    $hash_num[number] << num_str.join('').to_i
+    
+   end
+end
+
+br = 0
+arr = []
+File.open("C-small-practice.in") do |line|
+  line.each do |i|
+    arr << i.chomp.split(" ")
+  end
+end
+
+arr.shift[0].to_i
+(0...arr.size).each do |el|
+
+  a = arr[el][0].to_i
+  b = arr[el][1].to_i
+  
+(a..b).each do |i|
+  $hash_num = {}
+  (a..b).each do |j|
+    if (j > i)
+       split_and_shift(j, arr[el][0].length)
+       if($hash_num[j].include?(i))
+         br += 1
+       end
+    end
+  end
+end
+p "Case ##{el+1}: #{br}"
+br = 0
+end
+
